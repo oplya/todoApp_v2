1933ec050a100d21184f3cd7fbc58f28837bed50
Ivan <71104279+oplya@users.noreply.github.com>
2025-10-05 17:03:49 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import TodoForm from './components/Todos/TodoForm'
 import TodoList from './components/Todos/TodoList'
-// import DeleteMe from './components/Todos/DeleteMe'
-// Проверка изоляции классов из TodoForm.module.css
+import TodosActions from './components/Todos/TodosActions'
 import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([])
 
   const addTodoHandler = (text) => {
-    setTodos([...todos, text])
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    }
+
+    setTodos([...todos, newTodo])
   }
 
-  const deleteTodoHandler = (index) => {
-    setTodos(todos.filter((todo, idx) => idx !== index))
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo }
+      )
+    )
+  }
+
+  const resetTodosHandler = () => {
+    setTodos([])
+  }
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted))
   }
 
   return (
     <div className="App">
       <h1>Todo APP</h1>
       <TodoForm addTodo={addTodoHandler} />
+      <TodosActions
+        resetTodos={resetTodosHandler}
+        deleteCompletedTodos={deleteCompletedTodosHandler}
+      />
       <TodoList
         todos={todos}
         deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
       />
-      {/* <DeleteMe /> */}
     </div>
   )
 }
