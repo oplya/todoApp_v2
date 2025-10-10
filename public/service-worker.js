9ab36c7929e7ff2965d2dedffcc4c8d56cb8fa17
Ivan <71104279+oplya@users.noreply.github.com>
2025-10-10 16:19:11 +0300

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'todo-app-cache-v2'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon-32x32-v3.png',
+  '/site.manifest',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        )
+      )
+  )
+})
